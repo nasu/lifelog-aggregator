da09c5065fed22c504e97c4969da90e95a24c6d4
nasu-tatsuya <nasu.tatsuya@dena.jp>
2021-07-01 02:07:38 +0900

    WIP: google authentication

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/nasu/lifelog-aggregator
 
 go 1.13
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.3.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914
+	google.golang.org/api v0.49.0
+)
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"golang.org/x/oauth2"
+	v2 "google.golang.org/api/oauth2/v2"
 
 	"github.com/nasu/lifelog-aggregator/oura"
 	"github.com/nasu/lifelog-aggregator/toggl"
@@ -19,6 +26,31 @@ func init() {
 }
 
 func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/diary", diary)
+	e.GET("/auth/cb", authCb)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func diary(c echo.Context) error {
+	auth := c.Request().Header.Get(echo.HeaderAuthorization)
+	if auth == "" {
+		config := &oauth2.Config{
+			ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+			ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  "https://accounts.google.com/o/oauth2/v2/auth",
+				TokenURL: "https://www.googleapis.com/oauth2/v4/token",
+			},
+			Scopes:      []string{"openid", "profile", "email"},
+			RedirectURL: "http://localhost:8080/auth/cb",
+		}
+		state := "abcdefg"
+		return c.Redirect(http.StatusFound, config.AuthCodeURL(state, oauth2.AccessTypeOffline, oauth2.ApprovalForce))
+	}
+
 	now := time.Now()
 	total := make(map[string]map[string]int)
 	for i := 1; i <= 7; i++ {
@@ -27,7 +59,37 @@ func main() {
 		projectTimes["睡眠"] += fromOura(date)
 		total[date] = projectTimes
 	}
-	fmt.Println(total)
+	return c.String(http.StatusOK, fmt.Sprintf("%#v", total))
+}
+
+func authCb(c echo.Context) error {
+	config := &oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://accounts.google.com/o/oauth2/v2/auth",
+			TokenURL: "https://www.googleapis.com/oauth2/v4/token",
+		},
+		Scopes:      []string{"openid", "profile"},
+		RedirectURL: "http://localhost:8080/auth/cb",
+	}
+	ctx := context.Background()
+	token, err := config.Exchange(ctx, c.QueryParam("code"))
+	if err != nil {
+		return err
+	}
+	if !token.Valid() {
+		return fmt.Errorf("token is invalid")
+	}
+	service, err := v2.New(config.Client(ctx, token))
+	if err != nil {
+		return err
+	}
+	tokenInfo, err := service.Tokeninfo().AccessToken(token.AccessToken).Context(ctx).Do()
+	if err != nil {
+		return err
+	}
+	return c.String(http.StatusOK, fmt.Sprintf("%#v\n\n%s\n%s\n%s\n%s", tokenInfo, token.AccessToken, token.RefreshToken, token.Expiry, token.TokenType))
 }
 
 func fromToggle(date string) map[string]int {
